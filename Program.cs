c9a78c8d09f94b20043d1db74e405150c58d2e6a
fouadabujasser <fouad.abujasser@gmail.com>
2025-01-19 10:43:16 +0200

    Updated

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            //GithubCheck
+            //Updated:10:41
             string userChoice = "";
             List<int> myList = new List<int>() {4,5,5,5,5,5,5,5,5,5,5};
             Console.BackgroundColor = ConsoleColor.DarkRed;
